9c53727b590fd45e6ab9074168c3d57953a43912
Sagar More <sagar.more@pepperfry.com>
2018-10-27 18:42:20 +0530

    - Added SearchableItemAttributeSet. - Code refactored.

--- SiriShortcuts/SiriShortcuts/ViewController.swift
+++ SiriShortcuts/SiriShortcuts/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Intents
+import CoreSpotlight
 
 
 class ViewController: UIViewController {
@@ -16,14 +18,14 @@ class ViewController: UIViewController {
     }()
     
     private let userKey = "userDataKey"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupIntents()
     }
     
-    //setup the Siri intent to donate
+    // MARK: - Setup Intents
     func setupIntents() {
         //set activityType, which is defined in Info.plist file
         let activity = NSUserActivity(activityType: ViewController.siriIdentifier)
@@ -32,20 +34,23 @@ class ViewController: UIViewController {
         activity.userInfo = [userKey : "Hey there! You came here through Siri Shortcut."]
         // properties allow iOS to search and suggest our NSUserActivity on the device.
         activity.isEligibleForSearch = true
-        if #available(iOS 12.0, *) {
-            //Siri can suggest the user activity as a shortcut to the user
-            activity.isEligibleForPrediction = true
-            activity.persistentIdentifier = NSUserActivityPersistentIdentifier(ViewController.siriIdentifier)
-            //activity.suggestedInvocationPhrase = "Try this furniture?"
-        } else {
-            // Fallback on earlier versions
-        }
+        //Siri can suggest the user activity as a shortcut to the user
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = NSUserActivityPersistentIdentifier(ViewController.siriIdentifier)
+        
+        //create attributes for shortcut like image & description
+        let attributes = CSSearchableItemAttributeSet(itemContentType: "contentType")
+        attributes.thumbnailData = UIImage(named: "smile")!.pngData()
+        attributes.contentDescription = "Awesome shortcut!"
+        
+        activity.contentAttributeSet = attributes
+        activity.suggestedInvocationPhrase = "My Shortcut"
         //donate shortcut
         view.userActivity = activity
         activity.becomeCurrent()
     }
     
-    //show alert
+    // MARK: - Show Alert
     func showAlert(_ userInfo :  [AnyHashable : Any]?) {
         guard let userMessage = userInfo?[userKey] as? String else {
             return
@@ -54,6 +59,6 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-
+    
 }
 
